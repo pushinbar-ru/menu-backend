7649fbd135846b9589fabd82f949063e0fc16f9a
Гордиенко Артем Андреевич <gor.aa@skbkontur.ru>
2022-05-05 16:53:44 +0500

    feat: db context Configuration

--- MenuWebApi/Startup.cs
+++ MenuWebApi/Startup.cs
@@ -1,6 +1,7 @@
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -41,6 +42,9 @@ namespace Pushinbar.API
 
             services.AddTransient<KonturMarketClient>((context) => 
                 new KonturMarketClient(konturMarketOptions.ApiKey, konturMarketOptions.ShopId));
+            
+            services.AddDbContext<DBContext>(options =>
+                options.UseNpgsql(dbOptions.ConnectionString));
             
             services.AddTransient<AlcoholRepository>((context) => 
                 new AlcoholRepository(dbOptions.ConnectionString));
